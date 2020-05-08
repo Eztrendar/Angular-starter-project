470d6c8f92745189c140f0ebacdb03c4a45feedf
Eztrendar <petculescu.ionut93@gmail.com>
2020-05-08 17:16:03 +0300

    Added mock-up server interaction

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,6 +12,9 @@ import { MessageService } from './message.service'
 export class HeroService
 {
   private heroesUrl = 'api/heroes';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+  };
 
   constructor(private http: HttpClient,
               private messageService: MessageService) { }
@@ -34,6 +37,42 @@ export class HeroService
       catchError(this.handleError<Hero>(`getHero id=${id}`))
     );
   }
+
+  updateHero(hero: Hero): Observable<any>{
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`),
+      catchError(this.handleError<any>('updateHero')))
+    );
+  }
+
+  addHero(hero :Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero with id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  deleteHero(hero: Hero | number): Observable<Hero>{
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  searchHeroes(term: string): Observable<Hero[]>{
+    if(!term.trim()) return of([]);
+
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(x => x.length ?
+        this.log(`found heroes matching "${term}"`):
+        this.log(`no heroes matching "${term}"`),
+        catchError(this.handleError<Hero[]>('searchHeroes', []))
+    ));
+  }
+
 
   private log(message: string)
   {
